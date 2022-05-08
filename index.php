ad2457eddab02ba75b22ce642093da7d87775004
Abhijith KM <meabhijithkm@gmail.com>
2022-05-08 20:22:39 +0530

    changed .html links to .php in index.php

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
   <header id="header" class="header fixed-top">
     <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
 
-      <a href="index.html" class="logo d-flex align-items-center">
+      <a href="index.php" class="logo d-flex align-items-center">
         <img src="assets/img/apple-touch-icon.png" alt="">
         <span>duArise</span>
       </a>
@@ -1235,7 +1235,7 @@
       <div class="container">
         <div class="row gy-4">
           <div class="col-lg-5 col-md-12 footer-info">
-            <a href="index.html" class="logo d-flex align-items-center">
+            <a href="index.php" class="logo d-flex align-items-center">
               <img src="assets/img/logo.png" alt="">
               <span>EduArise</span>
             </a>
